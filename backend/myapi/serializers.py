97cc92c2d613acc91893a2b0d0f39d73d2e39e1b
agnealeknavicute <aleknavicute@gmail.com>
2024-01-18 15:17:24 +0200

    import error solved

--- backend/myapi/serializers.py
+++ backend/myapi/serializers.py
@@ -1,53 +1,6 @@
-from .models import Income
+from .models import IncExpModel
 from rest_framework import serializers
-
-
-# class IncomeSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Income
-#         fields = ['name', 'value']
-
-# class UpdatedIncomeListSerializer(serializers.ListSerializer):
-#     child = IncomeSerializer()
-
-# class UpdatedIncomeSerializer(serializers.Serializer):
-#     incomeItems = UpdatedIncomeListSerializer()
-
-#     def create(self, validated_data):
-#         income_items_data = validated_data.get('incomeItems')
-#         income_items = [Income.objects.create(**item) for item in income_items_data]
-#         return {'incomeItems': income_items}
-
-# class IncomeSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Income
-#         fields = ['name', 'value', 'type']
-
-# class UpdatedIncomeListSerializer(serializers.ListSerializer):
-#     child = IncomeSerializer()
-
-# class UpdatedIncomeSerializer(serializers.Serializer):
-#     incomeItems = UpdatedIncomeListSerializer()
-    
-
-# class UpdatedExpenseSerializer(serializers.Serializer):
-#     incomeItems = UpdatedExpenseListSerializer()
-
-#     def create(self, validated_data):
-#         income_items_data = validated_data.get('incomeItems')
-#         type_value = income_items_data[0].get('type', 'income')  # Assuming the 'type' is the same for all items
-
-#         # Remove 'type' from the dictionary to avoid conflict
-#         for item in income_items_data:
-#             item.pop('type', None)
-
-#         income_items = [Income.objects.create(type=type_value, **item) for item in income_items_data]
-#         return {'incomeItems': income_items}
-
-# serializers.py
-
 from rest_framework import serializers
-from .models import Income, Expense  # Make sure to import Expense here
 
 class ItemSerializer(serializers.Serializer):
     name = serializers.CharField()
@@ -66,14 +19,14 @@ class UpdatedIncomeSerializer(serializers.Serializer):
             item_type_value = income_items_data[0].get('type', 'income')
             for item in income_items_data:
                 item.pop('type', None)
-            income_objects = [Income.objects.create(type=item_type_value, **item) for item in income_items_data]
+            income_objects = [IncExpModel.objects.create(type=item_type_value, **item) for item in income_items_data]
             return {'incomeItems': income_objects, 'expenseItems': []}
 
         elif expense_items_data:
             item_type_value = expense_items_data[0].get('type', 'expense')
             for item in expense_items_data:
                 item.pop('type', None)
-            expense_objects = [Expense.objects.create(type=item_type_value, **item) for item in expense_items_data]
+            expense_objects = [IncExpModel.objects.create(type=item_type_value, **item) for item in expense_items_data]
             return {'incomeItems': [], 'expenseItems': expense_objects}
 
         else:
